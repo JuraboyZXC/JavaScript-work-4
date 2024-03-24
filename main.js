cf58b39d686043658367501e62b420324fbbd7eb
Maestro <gameacounto726@gmail.com>
2024-03-24 20:20:05 +0500

    JS work

---
+++ main.js
@@ -0,0 +1,67 @@
+// |+|
+let number1 = prompt("Write number 1")
+let number2 = prompt("Write number 2")
+
+console.log("Ответь  :", parseFloat(number1) + parseFloat(number2));
+
+// |-|
+let number3 = prompt("Write number 1")
+let number4 = prompt("Write number 2")
+
+console.log("Ответь  :", parseFloat(number3) - parseFloat(number4));
+
+// |*|
+let number5 = prompt("Write number 1")
+let number6 = prompt("Write number 2")
+
+console.log("Ответь  :", parseFloat(number5) * parseFloat(number6));
+
+// |/|
+let number7 = prompt("Write number 1")
+let number8 = prompt("Write number 2")
+
+console.log("Ответь  :", parseFloat(number7) / parseFloat(number8));
+
+// |+ -|
+let number9 = prompt("Write number 1")
+let number10 = prompt("Write number 2")
+let number11 = prompt("Write number 3")
+
+console.log("Ответь  :", parseFloat(number9) + parseFloat(number10) - parseFloat(number11));
+
+// |- +|
+let number12 = prompt("Write number 1")
+let number13 = prompt("Write number 2")
+let number14 = prompt("Write number 3")
+
+console.log("Ответь  :", parseFloat(number12) - parseFloat(number13) + parseFloat(number14));
+
+// |+ *|
+let number15 = prompt("Write number 1")
+let number16 = prompt("Write number 2")
+let number17 = prompt("Write number 3")
+
+console.log("Ответь  :", parseFloat(number15) + parseFloat(number16) * parseFloat(number17));
+
+// |- /|
+let number18 = prompt("Write number 1")
+let number19 = prompt("Write number 2")
+let number20 = prompt("Write number 3")
+
+console.log("Ответь  :", parseFloat(number18) - parseFloat(number19) / parseFloat(number20));
+
+// |+ / -|
+let number21 = prompt("Write number 1")
+let number22 = prompt("Write number 2")
+let number23 = prompt("Write number 3")
+let number24 = prompt("Write number 4")
+
+console.log("Ответь  :", parseFloat(number21) + parseFloat(number22) / parseFloat(number23) - parseFloat(number24));
+
+// |- * +|
+let number25 = prompt("Write number 1")
+let number26 = prompt("Write number 2")
+let number27 = prompt("Write number 3")
+let number28 = prompt("Write number 4")
+
+console.log("Ответь  :", parseFloat(number25) - parseFloat(number26) * parseFloat(number27) + parseFloat(number28));
